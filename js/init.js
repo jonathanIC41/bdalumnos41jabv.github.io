8ca98c28a3381c52183d4b2ad29afc13ad118e2c
jonathanIC41 <82475717+jonathanIC41@users.noreply.github.com>
2021-04-13 20:11:24 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -7,12 +7,12 @@
  * quedar igual que en tu
  * servidor. */
 // @ts-ignore
-firebase.initializeApp({
-  apiKey: "AIzaSyDjQZ58Whsg1097-ejejRw3U0Y993Q-1es",
+fvar firebaseConfig = {
+    apiKey: "AIzaSyDjQZ58Whsg1097-ejejRw3U0Y993Q-1es",
     authDomain: "bdalumic41jabv.firebaseapp.com",
     projectId: "bdalumic41jabv",
     storageBucket: "bdalumic41jabv.appspot.com",
     messagingSenderId: "1055574803467",
     appId: "1:1055574803467:web:6e2d40bd5ef028aa54b30e",
     measurementId: "G-2Q0WKTFKSW"
-});
+  };
